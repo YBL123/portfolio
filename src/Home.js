c7d96fd5fbdc9e81bb739484979cf9cbaa391505
YardenLawson <yardenlawson@gmail.com>
2020-06-16 18:22:46 +0100

    update

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// * Our basic home page
+
+const Home = () => (
+  <section className="">
+    <div className="">
+      <div className="">
+        <p className="">
+          <span role="img" aria-label="logo" className="logo-emoji">🧀</span>Home
+        </p>
+      </div>
+    </div>
+  </section>
+)
+
+export default Home
